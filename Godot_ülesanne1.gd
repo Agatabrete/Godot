7fb6d0689b8af29d8578ec6b13fb828c6684c5bb
Agatabrete <Agata-brete.jopiselg@hkhk.edu.ee>
2024-03-11 10:49:45 +0200

    praegune olukord

---
+++ Godot_ülesanne1.gd
@@ -0,0 +1,22 @@
+extends Node
+#Agata Brete Jõpiselg
+#11.03.2024
+#ülesanne 1
+
+func _ready():
+	var PlayerName = "Godot robot"
+	var pts = 0
+	var dmg = 0.5
+	print(PlayerName)
+	print(pts)
+	print(dmg)
+	var arv1 = 7
+	var arv2 = 2.5
+	print(round(arv1 / arv2))
+	print(sqrt(81))
+	print(pow(7,2))
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
